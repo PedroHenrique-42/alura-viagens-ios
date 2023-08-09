7b5bf8f585d9581db424069512e1c8b3840ba0a2
Pedro <pferezin@magnasistemas.com.br>
2023-08-09 09:15:06 -0300

    Initial Commit

---
+++ Viagens/ViagensApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViagensApp.swift
+//  Viagens
+//
+//  Created by Pedro Henrique on 09/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ViagensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
